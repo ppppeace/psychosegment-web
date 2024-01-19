3417fa99a703174de821a3060267508209a54ca3
ppppeace <nutyada1990@gmail.com>
2024-01-19 20:59:17 +0700

    edit footer

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -4,31 +4,33 @@ import LogoFooter from '@/assets/logoFooter.png';
 
 function Footer() {
     return (
-        <div className='bg-black text-white h-48 pt-8 px-14'>
-            <footer className='flex justify-between'>
-                <div className='grid grid-cols-6 pt-10 gap-x-20 text-base font-semibold'>
-                    <a href='#home' className='col-span-2'>
-                        HOME
-                    </a>
-                    <a href='#Analysis' className='col-span-2'>
-                        Analysis
-                    </a>
-                    <a href='#Sentinels' className='col-span-2'>
-                        Sentinels
-                    </a>
-                    <a href='#mbti-percentages' className='col-span-2'>
-                        MBTI Percentages
-                    </a>
-                    <a href='#Diplomats' className='col-span-2'>
-                        Diplomats
-                    </a>
-                    <a href='#Explorers' className='col-span-2'>
-                        Explorers
-                    </a>
-                </div>
+        <div className='footer'>
+            <div className='bg-black text-white h-48 pt-8 px-14'>
+                <footer className='flex justify-between'>
+                    <div className='grid grid-cols-6 pt-10 gap-x-20 text-base font-semibold'>
+                        <a href='#home' className='col-span-2'>
+                            HOME
+                        </a>
+                        <a href='#Analysis' className='col-span-2'>
+                            Analysis
+                        </a>
+                        <a href='#Sentinels' className='col-span-2'>
+                            Sentinels
+                        </a>
+                        <a href='#mbti-percentages' className='col-span-2'>
+                            MBTI Percentages
+                        </a>
+                        <a href='#Diplomats' className='col-span-2'>
+                            Diplomats
+                        </a>
+                        <a href='#Explorers' className='col-span-2'>
+                            Explorers
+                        </a>
+                    </div>
 
-                <Image src={LogoFooter} alt={'logo'} className='w-32 h-36' />
-            </footer>
+                    <Image src={LogoFooter} alt={'logo'} className='w-32 h-36' />
+                </footer>
+            </div>
         </div>
     );
 }
