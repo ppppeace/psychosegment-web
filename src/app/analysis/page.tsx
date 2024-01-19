9284646c696d22fef431c412a4c57f3cb2f738bf
ppppeace <nutyada1990@gmail.com>
2024-01-19 20:58:58 +0700

    page analysis

--- src/app/analysis/page.tsx
+++ src/app/analysis/page.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Navbar from '../components/navbar/Navbar';
+import Footer from '../components/footer/Footer';
+import Result from '../features/Result';
 
 type Props = {};
 
 const AnalysisPage = (props: Props) => {
-    return <div className='bg-[var(--alabaster)]'>Page</div>;
+    return (
+        <div className='bg-[var(--alabaster)]'>
+            <Navbar />
+            <Result />
+            <Footer />
+        </div>
+    );
 };
 
 export default AnalysisPage;
