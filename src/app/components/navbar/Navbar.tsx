946017ce077e9505f4e31339c984e732f9e08fd5
ppppeace <nutyada1990@gmail.com>
2024-02-12 14:47:20 +0700

    fix dropdown

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -7,14 +7,14 @@ function Navbar() {
     return (
         <header className='bg-[var(--black)] text-white py-2 px-8 flex justify-between text-base font-semibold '>
             <Image src={Logo} className='w-16 h-14' alt='Logo' />
-            <nav>
-                <ul className='list-none mt-4 menu flex flex-row'>
-                    <li>
-                        <a href='#home'>HOME</a>
-                    </li>
-                    <li>
-                        <a href='#mbti-percentages'>MBTI Percentages</a>
-                    </li>
+            <div>
+                <ul className='list-none  menu flex flex-row'>
+                    <a href='#home'>
+                        <li>HOME</li>
+                    </a>
+                    <a href='#mbti-percentages'>
+                        <li>MBTI Percentages</li>
+                    </a>
 
                     <li>
                         {/* <DropDown /> */}
@@ -23,33 +23,22 @@ function Navbar() {
                             <Image src={Down} className='w-6 h-6' alt='' />
                         </button>
                         <ul className='menu-sub'>
-                            <li>
-                                <a href='#Analysis'>Analysis</a>
-                            </li>
-                            <li>
-                                <a href='#Diplomats'>Diplomats</a>
-                            </li>
-                            <li>
-                                <a href='#Sentinels'>Sentinels</a>
-                            </li>
-                            <li>
-                                <a href='#Explorers'>Explorers</a>
-                            </li>
+                            <a href='#Analysis'>
+                                <li>Analysis</li>
+                            </a>
+                            <a href='#Diplomats'>
+                                <li>Diplomats</li>
+                            </a>
+                            <a href='#Sentinels'>
+                                <li>Sentinels</li>
+                            </a>
+                            <a href='#Explorers'>
+                                <li>Explorers</li>
+                            </a>
                         </ul>
                     </li>
                 </ul>
-                {/* <div className='dropdown'>AnalysisDiplomatsSentinelsExplorers
-                    <button className='dropbtn'>
-                        Personalized Sales
-                        <Image src={Down} className='w-6 h-6' alt='' />
-                    </button>
-                    <div className='dropdown-content'>
-                        <a href='#personalized-sales'>Link 1</a>
-                        <a href='#personalized-sales'>Links 2</a>
-                        <a href='#personalized-sales'>Link 3</a>
-                    </div>
-                </div> */}
-            </nav>
+            </div>
         </header>
     );
 }
