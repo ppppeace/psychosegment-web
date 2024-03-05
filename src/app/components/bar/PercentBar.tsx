af87212fd96a69d7d6af79f062eef9880e3c3b12
ppppeace <nutyada1990@gmail.com>
2024-03-05 13:57:55 +0700

    create percent bar

--- src/app/components/bar/PercentBar.tsx
+++ src/app/components/bar/PercentBar.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
 
-const ProgressBar = (props: { bgcolor: any; completed: any }) => {
-    const { bgcolor, completed } = props;
+const PercentBar = ({ percent }: any) => {
+    const result = percent - 50;
     return (
         <div>
-            <div>
-                <span>{`${completed}%`}</span>
+            {/* <div className='text-white text-base'>+{result}%</div> */}
+            <div className='percent-bar-container'>
+                <div className='percent-bar' style={{ width: `${percent}%` }}></div>
             </div>
         </div>
     );
 };
 
-export default ProgressBar;
+export default PercentBar;
--- src/app/features/Percent.tsx
+++ src/app/features/Percent.tsx
@@ -6,12 +6,7 @@ import Manup from '@/assets/manup.png';
 import Man from '@/assets/man.png';
 
 import ProgressBar from '../components/bar/PercentBar';
-
-const testData = [
-    { bgcolor: '#6a1b9a', completed: 60 },
-    { bgcolor: '#00695c', completed: 30 },
-    { bgcolor: '#ef6c00', completed: 53 }
-];
+import PercentBar from '../components/bar/PercentBar';
 
 function Percent() {
     return (
@@ -19,6 +14,7 @@ function Percent() {
             <h2 className='text-4xl text-center' id='mbti-percentages'>
                 Thai MBTI Percentages Unveiled
             </h2>
+
             <ul className='mt-10 grid grid-cols-5 gap-4'>
                 <li className='mt-5 col-span-2'>
                     <Image src={Map} alt='logo' className='w-full h-4/5' />
@@ -50,11 +46,27 @@ function Percent() {
                         </div>
                     </div>
                 </li>
-                <li>
+                <li className='mt-16 text-[var(--red-alfa)] text-base'>
                     <div>
-                        {testData.map((item, idx) => (
-                            <ProgressBar key={idx} bgcolor={item.bgcolor} completed={item.completed} />
-                        ))}
+                        <div>+1.67%</div>
+                        <PercentBar percent={51.67} />
+                    </div>
+                    <div className='mt-32 '>
+                        <div className='text-right'>+3.17%</div>
+                        <div className='right-more'>
+                            <PercentBar percent={53.17} />
+                        </div>
+                    </div>
+                    <div className='mt-32'>
+                        <div>+13.11%</div>
+                        <PercentBar percent={63.11} />
+                    </div>
+
+                    <div className='mt-32 '>
+                        <div className='text-right'>+0.81%</div>
+                        <div className='right-more'>
+                            <PercentBar percent={50.81} />
+                        </div>
                     </div>
                 </li>
                 <li className='mt-5 text-center'>
