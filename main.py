e4217b03636e547a04cb9a6174e36e042301d487
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-04-08 21:46:00 +0700

    naivebay

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from typing import Optional, Union
 from fastapi import FastAPI
 from pydantic import BaseModel
+import pickle
 
 app = FastAPI()
+
+with open('naive_bayes_classifier.pkl', 'rb') as file:
+    naive_bayes_classifier = pickle.load(file)
+
+# Load the CountVectorizer
+with open('vectorizer.pkl', 'rb') as file:
+    vectorizer = pickle.load(file)
 
 class User(BaseModel):
     username: str
     password: str
     level: Optional[str] = "normal"
+
+@app.get("/json/{data}")
+def read_item(data: str):
+    sentence_to_predict = [data]
+    sentence_to_predict_vectorized = vectorizer.transform(sentence_to_predict)
+    predicted_category = naive_bayes_classifier.predict(sentence_to_predict_vectorized)
+    print("ประเภทของประโยค:", predicted_category)
+    return {"data is ": predicted_category}
 
 
 # http://127.0.0.1:3000/docs
@@ -20,9 +36,13 @@ def testjson():
     # return  {"message": "This is data from FastAPI"}
     return "hello my name is hwak"
 
-@app.get("/json/{data}")
-def read_item(data: str):
-    return {"data is ": data}
+# @app.get("/json/{data}")
+# def read_item(data: str):
+#     sentence_to_predict = [data]
+#     sentence_to_predict_vectorized = vectorizer.transform(sentence_to_predict)
+#     predicted_category = naive_bayes_classifier.predict(sentence_to_predict_vectorized)
+#     print("ประเภทของประโยค:", predicted_category)
+#     return {"data is ": predicted_category}
 
 @app.get("/hi")
 def hi(name:str):
